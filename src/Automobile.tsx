2e621d1cb4503b595934f142913e938d75f61331
Mike-casper <misha7709123@gmail.com>
2022-10-15 18:34:15 +0300

    I must to add 1-3

---
+++ src/Automobile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type CarComponentType = {
+    CarsArray: Array<CarsPropsType>
+}
+type CarsPropsType = {
+    manufacturer: string
+    model: string
+}
+export const Automobile = (props: CarComponentType) => {
+    return (
+        <table>
+            {props.CarsArray.map((objectFromCarsArray, index: number) => {
+                return (
+                    <tr>
+                        {/*<th>{objectFromCarsArray.}</th>*/}
+                        <th>{objectFromCarsArray.manufacturer}</th>
+                        <th>{objectFromCarsArray.model}</th>
+
+                    </tr>
+                )
+            })}
+                {/*<th>{props.CarsArray[0].manufacturer}</th>*/}
+                {/*<th>{props.CarsArray[1].manufacturer}</th>*/}
+                {/*<th>{props.CarsArray[2].manufacturer}</th>*/}
+                {/*<td>{props.CarsArray[0].model}</td>*/}
+                {/*<td>{props.CarsArray[1].model}</td>*/}
+                {/*<td>{props.CarsArray[2].model}</td>*/}
+        </table>
+    )
+}
